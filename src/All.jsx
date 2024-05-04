e8cf60568a418009c35ba82aa77e2436e6b4779d
Nikhil-KIET <95426399+Nikhil-KIET@users.noreply.github.com>
2024-05-04 22:43:04 +0530

    Add files via upload

---
+++ src/All.jsx
@@ -0,0 +1,170 @@
+function All(){
+    return(
+        <div>
+        <nav className="flex justify-evenly items-center ">
+            <img className="w-[200px]" src="https://imgs.search.brave.com/a0dcBucgh1fpQAJ3G1wau72HXvQ0YYo_7RLxzrsp-wU/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZy/ZWVwaWsuY29tLzI1/Ni8zMTM1LzMxMzU3/MTUucG5nP3NlbXQ9/YWlzX2h5YnJpZA" alt="" />
+            <div className=" border-b-4 flex justify-evenly items-center">
+            <div className="  w-[20%] h-[100%]">
+            <h1>JESSICA </h1>
+            <h1>GREENE</h1>
+            <p>FREELANCE WRITER</p>
+            </div>
+            <div className="w-[50%] space-y-3 border-l-4 p-5 ">
+                <p>I'm an award-winning content writer who has eight years of experience creating compelling articles and short stories. I'm continuously searching for new topics and stories to capture the attention of new readers. With my knowledge and experience, I can help you fulfill your content creation goals.</p>
+                <p>Email :xyz@geekster.in</p>
+                <p>Website: geekster.in</p>
+            </div>
+            </div>
+            
+            
+
+        </nav>
+
+        <section className="flex w-screen py-5 ">
+        
+        <img className="w-[200px]" src="https://imgs.search.brave.com/LKb0z0j6cLRUOee_L1Eezt0VZz3pLrVQ8uC3iw9oylU/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZy/ZWVwaWsuY29tLzI1/Ni8xMDg5Ny8xMDg5/NzgyNi5wbmc_c2Vt/dD1haXNfaHlicmlk" alt="" />
+        <div className=" border-b-4">
+            <div>
+            <div className=" flex gap-20  px-16  w-screen">
+                <h1>Present</h1>
+                <p>
+                <p>Jessica Green Mrketing -Freelance Writer</p>
+
+<p>Create blog posts and lending pages,ebboks amdcase studies forclients </p>
+                </p>
+            </div>
+            <div className=" flex  gap-23 px-16  w-screen">
+                <h1>2016</h1>
+                <p>
+                <p>Jessica Green Mrketing -Freelance Writer</p>
+
+<p>Create blog posts and lending pages,ebboks amdcase studies forclients </p>
+                </p>
+            </div>
+            <div className=" flex    px-16  w-screen">
+                <h1>2013-2015</h1>
+                <p>
+                <p>Jessica Green Mrketing -Freelance Writer</p>
+
+<p>Create blog posts and lending pages,ebboks amdcase studies forclients </p>
+                </p>
+            </div>
+            </div>
+        </div>
+
+        </section>
+
+        <main className=" flex justify-start w-screen ">
+
+            <img className="w-[200px]" src="https://imgs.search.brave.com/U2x8PdMdeucQkF05CqRTAvzoy9lIg5ep8oFRlbIT6UE/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4z/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvYnVzaW5lc3Mt/b2ZmaWNlLTExLzUx/Mi9CdXNpbmVzc19P/ZmZpY2VfY2lyY2xl/X2JhZ193b3JrX3By/b2ZpbGUtNTEyLnBu/Zw" alt="" />
+            <div className=" border-b-4 w-[100%] justify-start">
+            <div className="flex items-center justify-start   w-[30%]">
+            <h1>Writing</h1>
+            <div className="flex gap-2">
+            <div className=" rounded-full bg-blue-600 w-[20px] h-[20px]"></div>
+            <div className=" rounded-full bg-blue-600 w-[20px] h-[20px]"></div>
+            <div className=" rounded-full bg-blue-600 w-[20px] h-[20px]"></div>
+            <div className=" rounded-full bg-blue-600 w-[20px] h-[20px]"></div>
+            <div className=" rounded-full bg-blue-600 w-[20px] h-[20px]"></div>
+            </div>
+            </div>
+            <br />
+            
+
+
+            
+            <div className="flex items-center justify-start   w-[30%]">
+            <h1>Editing</h1>
+            <div className="flex gap-2">
+            <div className=" rounded-full bg-blue-600 w-[20px] h-[20px]"></div>
+            <div className=" rounded-full bg-blue-600 w-[20px] h-[20px]"></div>
+            <div className=" rounded-full bg-blue-600 w-[20px] h-[20px]"></div>
+            
+            </div>
+            
+            </div>
+            <br />
+            
+
+        
+            <div className="flex items-center justify-start  w-[27%]">
+            <h1>Social Media</h1>
+            <div className="flex gap-2">
+            
+            <div className=" rounded-full bg-blue-600 w-[20px] h-[20px]"></div>
+            <div className=" rounded-full bg-blue-600 w-[20px] h-[20px]"></div>
+            <div className=" rounded-full bg-blue-600 w-[20px] h-[20px]"></div>
+            <div className=" rounded-full bg-blue-600 w-[20px] h-[20px]"></div>
+            <div className=" rounded-full bg-blue-600 w-[20px] h-[20px]"></div>
+            </div>
+            </div>
+            <br />
+            
+
+
+            
+            <div className="flex items-center justify-start  w-[0%]">
+            
+            <h1>Researching</h1>
+            <div className="flex gap-2 justify-start">
+            <div className=" rounded-full bg-blue-600 w-[20px] h-[20px]"></div>
+            <div className=" rounded-full bg-blue-600 w-[20px] h-[20px]"></div>
+            <div className=" rounded-full bg-blue-600 w-[20px] h-[20px]"></div>
+            <div className=" rounded-full bg-blue-600 w-[20px] h-[20px]"></div>
+            
+            
+            </div>
+            <br />
+            </div>
+            
+            
+            <div className="flex items-center  w-[45%]  ">
+            
+            <h1>Seo</h1>
+            <div className="flex gap-2 ">
+            <div className=" rounded-full bg-blue-600 w-[20px] h-[20px]"></div>
+            <div className=" rounded-full bg-blue-600 w-[20px] h-[20px]"></div>
+            <div className=" rounded-full bg-blue-600 w-[20px] h-[20px]"></div>
+            <div className=" rounded-full bg-blue-600 w-[20px] h-[20px]"></div>
+            <div className=" rounded-full bg-blue-600 w-[20px] h-[20px]"></div>
+            </div>
+            </div>
+            </div>
+            <br />
+
+            <div className="w-[50%] h-[80%]">
+                <div>
+                    <h1>Zapier</h1>
+                    <p> Orgainse youyr life with the agilemethod</p>
+                </div>
+                <br />
+                <div>
+                    <h1>Zapier</h1>
+                    <p> Orgainse youyr life with the agilemethod</p>
+                </div>
+                <br />
+                <div>
+                    <h1>Zapier</h1>
+                    <p> Orgainse youyr life with the agilemethod</p>
+                </div>
+
+            </div>
+
+            
+
+        </main>
+        
+        <footer className="flex justify-between ">
+            <img className="w-[150px]" src="https://imgs.search.brave.com/7Uaq1Xpwr10qeuUf_-tV6T1Ah_M0eClbuGWR3tuvj10/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvc29jaWFsLW1l/c3NhZ2luZy11aS1j/b2xvci1zaGFwZXMv/MTI4L3Rvb2xzLWNp/cmNsZS1ibHVlLTUx/Mi5wbmc" alt="" />
+            <img className="w-[200px]" src="https://imgs.search.brave.com/jJZc0XkxbM05le2LwAvWb99FVVuoRqJkEUJnXcV27jA/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pLnN0/YWNrLmltZ3VyLmNv/bS8xNmlzNC5wbmc" alt="" />
+            <img className="w-[150px]" src="https://imgs.search.brave.com/U2x8PdMdeucQkF05CqRTAvzoy9lIg5ep8oFRlbIT6UE/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4z/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvYnVzaW5lc3Mt/b2ZmaWNlLTExLzUx/Mi9CdXNpbmVzc19P/ZmZpY2VfY2lyY2xl/X2JhZ193b3JrX3By/b2ZpbGUtNTEyLnBu/Zw" alt="" />
+            <img  className="w-[150px]"src="https://imgs.search.brave.com/a0dcBucgh1fpQAJ3G1wau72HXvQ0YYo_7RLxzrsp-wU/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZy/ZWVwaWsuY29tLzI1/Ni8zMTM1LzMxMzU3/MTUucG5nP3NlbXQ9/YWlzX2h5YnJpZA" alt="" />
+            <img className="w-[150px]" src="https://imgs.search.brave.com/LKb0z0j6cLRUOee_L1Eezt0VZz3pLrVQ8uC3iw9oylU/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZy/ZWVwaWsuY29tLzI1/Ni8xMDg5Ny8xMDg5/NzgyNi5wbmc_c2Vt/dD1haXNfaHlicmlk" alt="" />
+            
+
+        </footer>
+    </div>
+    
+    )
+}
+export default All;
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import All from "./All";
+
+function App() {
+  return (
+    <All></All>
+    
+  );
+}
+
+export default App;
